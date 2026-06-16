ba2d6b382fbad3712820bc412be4e1a06ddf5d19
ali ahsan <aloliking2004@gmail.com>
2026-06-16 06:44:07 +0300

    Launch BranchesForm on startup

---
+++ University Timetable and Classroom Management System/PresentationLayer/BranchesForm.cs
@@ -0,0 +1,10 @@
+namespace University_Timetable_and_Classroom_Management_System
+{
+    public partial class BranchesForm
+    {
+        public BranchesForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
